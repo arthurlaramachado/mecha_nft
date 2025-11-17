d550680273d8a7702323e227ccc70c630ca26d5b
Arthur de Lara Machado <arthurlaramachado@gmail.com>
2025-11-17 17:19:50 -0300

    add: Minting tests

---
+++ test/MechaMintingTest.t.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./MechaTest.t.sol";
+
+contract MechaMintingTest is MechaTest {
+    uint256 amount;
+
+    function setUp() public override {
+        super.setUp();
+        amount = mecha.MINT_PRICE();
+    }
+
+    function testMintWithRightPrice() public {
+        vm.prank(user1);
+        uint256 tokenId = _mintTo(amount);
+
+        assertEq(mecha.ownerOf(tokenId), user1);
+        assertEq(mecha.balanceOf(user1), 1);
+    }
+    
+    function testMintHigherAmountRevert() public {
+        vm.prank(user1);
+        vm.expectRevert("Invalid amount of ether");
+
+        _mintTo(amount + 1);
+    }
+
+    function testMintLowerAmountRevert() public {
+        vm.prank(user1);
+        vm.expectRevert("Invalid amount of ether");
+        
+        _mintTo(0);
+    }
+
+    function testMintTokenAttributesCreated() public {
+        vm.prank(user1);
+        uint256 tokenId = _mintTo(mecha.MINT_PRICE());
+        Mecha.MechaAttributes memory attr = mecha.getAttributes(tokenId);
+
+        assertGe(attr.strength, 1);
+        assertLe(attr.strength, 99);
+
+        assertGe(attr.health, 1);
+        assertLe(attr.health, 99);
+
+        assertGe(attr.speed, 1);
+        assertLe(attr.speed, 99);
+    }
+
+    function testMintEventTransferValid() public {
+        vm.prank(user1);
+        vm.expectEmit();
+        emit Transfer(address(0), address(user1), 1);
+
+        _mintTo(amount);
+    }
+
+    function testMintEventMechaMintedValid() public {
+        vm.prank(user1);
+        vm.expectEmit(true, true, false, false);
+        emit MechaMinted(user1, 1, 0, 0, 0, 0);
+
+        _mintTo(amount);
+    }
+
+    function testSameUserMintingIds() public {
+        vm.prank(user1);
+        uint256 tokenId1 = _mintTo(amount);
+
+        vm.prank(user1);
+        uint256 tokenId2 = _mintTo(amount);
+
+        assertEq(tokenId1, 1);
+        assertEq(tokenId2, 2);
+    }
+
+    function testMultipleUsersMintingIds() public {
+        vm.prank(user1);
+        uint256 tokenId1 = _mintTo(amount);
+
+        vm.prank(user2);
+        uint256 tokenId2 = _mintTo(amount);
+
+        vm.prank(user3);
+        uint256 tokenId3 = _mintTo(amount);
+
+        assertEq(tokenId1, 1);
+        assertEq(tokenId2, 2);
+        assertEq(tokenId3, 3);
+    }
+
+    function testMintedAmount() public {
+        vm.prank(user1);
+        _mintTo(amount);
+
+        vm.prank(user2);
+        _mintTo(amount);
+
+        vm.prank(user1);
+        _mintTo(amount);
+
+        uint256 numOfUser1Tokens = mecha.balanceOf(user1);
+        uint256 numOfUser2Tokens = mecha.balanceOf(user2);
+        uint256 numOfTotalTokens = mecha.getActiveTokens();
+        
+        assertEq(numOfUser1Tokens, 2);
+        assertEq(numOfUser2Tokens, 1);
+        assertEq(numOfTotalTokens, 3);
+    }
+}
---
+++ test/MechaTest.t.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//Forge can re-run your tests when you make changes to your files using forge test --watch.
+//By default, only changed test files are re-run. If you want to re-run all tests on a change, you can use forge test --watch --run-all.
+
+import "forge-std/Test.sol";
+import "../src/Mecha.sol";
+
+contract MechaTest is Test {
+    Mecha public mecha;
+    address public user1 = address(0x1);
+    address public user2 = address(0x2);
+    address public user3 = address(0x3);
+
+    event Transfer(
+        address indexed _from, 
+        address indexed _to, 
+        uint256 indexed _tokenId
+    );
+
+    event Approval(
+        address indexed _owner, 
+        address indexed _approved, 
+        uint256 indexed _tokenId
+    );
+
+    event ApprovalForAll(
+        address indexed _owner, 
+        address indexed _operator, 
+        bool _approved
+    );
+
+    event MechaMinted(
+        address indexed minter,
+        uint256 indexed tokenId,
+        uint8 strength,
+        uint8 health,
+        uint8 speed,
+        uint256 mintPrice
+    );
+    
+    function setUp() virtual public {
+        mecha = new Mecha();
+        vm.deal(user1, 10 ether);
+        vm.deal(user2, 10 ether);
+        vm.deal(user3, 10 ether);
+    }
+
+    /**
+    *   @dev Pranks the user to mint an NFT
+    *   @param amount The amount to try minting the NFT
+    */
+    function _mintTo(uint256 amount) internal returns (uint256 tokenId) {
+        tokenId = mecha.mint{value: amount}();
+    }
+}
